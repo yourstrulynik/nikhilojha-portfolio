f7e6be8b8549da030d5f7a1bc32711b1358cdeb6
yourstrulynik <nikhilojha05@gmail.com>
2024-04-22 01:31:30 +0530

    about change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,11 +21,11 @@ export default function About() {
 
           <div>
             <p>
-              I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?
+              &quot;I am a skilled software developer with a strong focus on
+              front-end development. I have a quick learning ability and I am
+              currently expanding my expertise to back-end development to become
+              a full-stack developer. If you had a software expert available at
+              your fingertips, what would you do?&quot;
             </p>
           </div>
         </div>
